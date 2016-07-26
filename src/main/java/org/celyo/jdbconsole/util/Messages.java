7729f238ebdf235f23355b5224f752e0a06f9e33
celyo <celyo@mail.bg>
2016-07-26 18:20:03 +0300

    implemented internationalization (messages.properties)

--- src/main/java/org/celyo/jdbconsole/util/Messages.java
+++ src/main/java/org/celyo/jdbconsole/util/Messages.java
@@ -13,26 +13,32 @@
  * the License.
  * 
  */
-
 package org.celyo.jdbconsole.util;
 
+import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-   private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
-   private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
-   
-   
-   private static String getString(String key) {
-      try {
-         return BUNDLE.getString(key);
-      } catch (MissingResourceException e) {
-         return key;
-      }
-   }
-   
-   public static String getString(MessageKey key) {
-      return getString(key.getKey());
-   }  
+
+  private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
+  private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+  private static String getString(String key) {
+    try {
+      return BUNDLE.getString(key);
+    } catch (MissingResourceException e) {
+      return key;
+    }
+  }
+
+  public static String getString(MessageKey key, Object... params) {
+    String message = getString(key.getKey());
+    
+    if (params != null && params.length > 0) {
+      return MessageFormat.format(message, params);
+    } else {
+      return message;
+    }
+  }
 }
